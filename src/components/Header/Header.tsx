56e2e9a2211eb66eaf8ecf82ca298f39c660e9b5
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-04-08 16:41:16 +0100

    created header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,32 @@
 import Logo from '../../assets/images/logo.png';
+import BgImg from '../../assets/images/header-banner.png';
 const Header = () => {
   return (
-    <div className="flex flex-col justify-center items-center mx-auto w-full h-[150px] bg-slate-500 py-10 transition-all ease-in-out duration-300">
+    <div className="flex flex-col justify-center items-center mx-auto w-full h-[150px] bg-slate-500 py-10 transition-all ease-in-out duration-300 bg-head-img">
       <div className="max-w-6xl flex flex-row">
-        <a className="text-xl p-2 bg-white rounded-full min-w-[100px] text-center hover:bg-[#63e31e] transition-all ease-in-out duration-300">
-          About
+        <div className="flex flex-col justify-center items-center mr-8">
+          <a
+            href="#"
+            className="text-xl p-2 bg-white rounded-full min-w-[100px] text-center hover:bg-[#63e31e] transition-all ease-in-out duration-300"
+          >
+            About
+          </a>
+        </div>
+        <a href="/">
+          <img
+            src={Logo}
+            alt="CampZilla"
+            className="max-h-[120px] w-auto hover:drop-shadow-xl	hover:hue-rotate-30	"
+          />
         </a>
-        <img src={Logo} alt="CampZilla" className="" />
-        <a className="text-xl p-2 bg-white rounded-full min-w-[100px] text-center hover:bg-[#63e31e] transition-all ease-in-out duration-300">
-          Contact
-        </a>
+        <div className="flex flex-col justify-center items-center ml-8">
+          <a
+            href="#"
+            className="text-xl p-2 bg-white rounded-full min-w-[100px] text-center hover:bg-[#63e31e] transition-all ease-in-out duration-300"
+          >
+            Contact
+          </a>
+        </div>
       </div>
     </div>
   );
