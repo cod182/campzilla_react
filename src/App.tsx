807c3111e185201e9ed9243827b74d48880945c5
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-04-08 16:51:54 +0100

    added mobile detection for side nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
+import { useState, useEffect } from 'react';
+
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { Home } from './routes/index';
 import { Header } from './components/index';
+import useWindowDimensions from './utils/useWindowDimensions';
 
 import './App.css';
 
 function App() {
+  const [mobile, setmobile] = useState(false);
+  const { width } = useWindowDimensions();
+  useEffect(() => {
+    if (width <= 450) {
+      setmobile(true);
+    } else {
+      setmobile(false);
+    }
+  }, [width]);
+
   return (
     <>
-      <Header />
+      {mobile && ''} <Header mobile={mobile} />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,19 @@
 import Logo from '../../assets/images/logo.png';
 import BgImg from '../../assets/images/header-banner.png';
-const Header = () => {
+const Header = (mobile: boolean) => {
   return (
     <div className="flex flex-col justify-center items-center mx-auto w-full h-[150px] bg-slate-500 py-10 transition-all ease-in-out duration-300 bg-head-img">
       <div className="max-w-6xl flex flex-row">
-        <div className="flex flex-col justify-center items-center mr-8">
-          <a
-            href="#"
-            className="text-xl p-2 bg-white rounded-full min-w-[100px] text-center hover:bg-[#63e31e] transition-all ease-in-out duration-300"
-          >
-            About
-          </a>
-        </div>
+        {!mobile && (
+          <div className="flex flex-col justify-center items-center mr-8">
+            <a
+              href="#"
+              className="text-xl p-2 bg-white rounded-full min-w-[100px] text-center hover:bg-[#63e31e] transition-all ease-in-out duration-300"
+            >
+              About
+            </a>
+          </div>
+        )}
         <a href="/">
           <img
             src={Logo}
@@ -19,14 +21,17 @@ const Header = () => {
             className="max-h-[120px] w-auto hover:drop-shadow-xl	hover:hue-rotate-30	"
           />
         </a>
-        <div className="flex flex-col justify-center items-center ml-8">
-          <a
-            href="#"
-            className="text-xl p-2 bg-white rounded-full min-w-[100px] text-center hover:bg-[#63e31e] transition-all ease-in-out duration-300"
-          >
-            Contact
-          </a>
-        </div>
+
+        {!mobile && (
+          <div className="flex flex-col justify-center items-center ml-8">
+            <a
+              href="#"
+              className="text-xl p-2 bg-white rounded-full min-w-[100px] text-center hover:bg-[#63e31e] transition-all ease-in-out duration-300"
+            >
+              Contact
+            </a>
+          </div>
+        )}
       </div>
     </div>
   );
