fa9c1fccd6a6b52bcae8e38f40399089cc6d3730
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-04-09 02:22:28 +0100

    added styling to about section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="mx-auto max-w-3xl bg-slate-600 h-[200px] rounded-2xl">
-      <div className="rounded-t-2xl bg-white bg-head-img"> About</div>
+    <div className="mx-auto max-w-3xl bg-slate-600 h-fit rounded-2xl">
+      <div className="rounded-t-2xl bg-white bg-head-img bg-bottom bg-contain text-center h-[100px] flex flex-row justify-center items-center text-2xl font-bold">
+        <h3 className="bg-white rounded-xl w-fit h-fit py-2 px-4">About</h3>
+      </div>
+      <div className="p-5">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam
+          veritatis, velit veniam modi aperiam vero hic iure doloribus fugit id
+          odit illum beatae commodi explicabo consectetur suscipit facere
+          perferendis! Asperiores.
+        </p>
+      </div>
     </div>
   );
 };
