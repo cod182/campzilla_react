0ac05e91bcbb7c4073364403798d78ad7f9c9202
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-04-20 12:43:12 +0100

    added view more bottom and state

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,59 +1,104 @@
+import { useState } from 'react';
 import { getMilesFromMeters } from '../../utils/useGetMiles';
+import { FcGoogle } from 'react-icons/fc';
+import { BsApple } from 'react-icons/bs';
+import { AiFillDownCircle } from 'react-icons/ai';
 
 const Result = ({ resultData }: { resultData: any }) => {
+  const [viewMore, setViewMore] = useState(false);
+
   console.log(resultData);
   return (
-    <section className="w-full min-h-[300px] h-auto flex flex-row justify-start items-center rounded-lg bg-gray-200 p-5">
-      <div>
-        <h2>{resultData.title}</h2>
-        <p>
-          Distance: <span>{getMilesFromMeters(resultData.distance)}</span> Miles
-        </p>
-
-        {resultData?.contacts
-          ? resultData?.contacts.map((contact: any) =>
-              contact?.phone
-                ? contact?.phone.map((phone: any) => {
-                    return (
-                      <p>
-                        Phone:{' '}
-                        <a
-                          className="hover:text-[#4ab836]"
-                          href={`tel:+${phone.value}`}
-                        >
-                          {phone.value}
-                        </a>
-                      </p>
-                    );
-                  })
-                : null
-            )
-          : null}
-
-        {resultData?.contacts
-          ? resultData?.contacts.map((contact: any) =>
-              contact?.fax
-                ? contact?.fax.map((fax: any) => {
-                    return <p>Fax: {fax.value}</p>;
-                  })
-                : null
-            )
-          : null}
-        {resultData?.contacts?.[0]?.www ? (
-          <p>
-            Website:{' '}
-            <a
-              className="hover:text-[#4ab836]"
-              href={resultData?.contacts?.[0]?.www?.[0]?.value}
-              target="_blank"
-              rel="noreferrer"
-            >
-              {resultData?.contacts?.[0]?.www?.[0]?.value}
-            </a>
-          </p>
-        ) : null}
+    <section
+      className={`w-full min-h-[300px] flex flex-col justify-start rounded-lg bg-gray-200 p-5 relative ${
+        viewMore ? 'max-h-[1000px]' : 'max-h-[500px]'
+      }`}
+    >
+      <div className="w-full h-auto group mb-4">
+        <a
+          href="#"
+          className=" text-[#23abff] group-hover:text-[#4ab836] sm:text-[30px] md:text-[50px] lg:text-[45px] font-semibold max-w-full ease-in-out transition-all duration-300"
+        >
+          {resultData.title}
+        </a>
+        <div className="border-b-4 border-[#23abff] group-hover:border-[#4ab836] w-[40px] group-hover:w-full transition-all ease-ine-out duration-1000"></div>
       </div>
-      <div></div>
+      <div className="flex flex-row justify-start items-center rounded-lg">
+        <div className="w-[50%]">
+          <p>
+            Distance: <span>{getMilesFromMeters(resultData.distance)}</span>
+            Miles
+          </p>
+
+          <div>
+            <p>
+              Phone:&nbsp;
+              <a
+                className="hover:text-[#4ab836] ase-in-out transition-all duration-300"
+                href={`tel:+${resultData?.contacts?.[0]?.phone?.[0].value}`}
+              >
+                {resultData?.contacts?.[0]?.phone?.[0].value}
+              </a>
+            </p>
+            {resultData?.contacts?.[0]?.www ? (
+              <p>
+                Website:&nbsp;
+                <a
+                  className="hover:text-[#4ab836] ase-in-out transition-all duration-300"
+                  href={resultData?.contacts?.[0]?.www?.[0]?.value}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {resultData?.contacts?.[0]?.www?.[0]?.value}
+                </a>
+              </p>
+            ) : null}
+          </div>
+
+          <div className="my-2">
+            <h4 className="font-semibold">Address:</h4>
+
+            <address>
+              <p>{resultData?.title}</p>
+              <p>{resultData?.address?.city}</p>
+              <p>{resultData?.address?.county}</p>
+              <p>{resultData?.address?.postalCode}</p>
+            </address>
+            <div className="flex flex-row jusitfy-space items-center">
+              <h5>Navigate:</h5>
+
+              <a
+                href={`https://maps.google.com/?q=${resultData?.title},${resultData?.address?.city},${resultData?.address?.county},${resultData?.address?.postalCode}`}
+                target="_blank"
+                rel="noreffer"
+                className="w-[25px] h-[25px] flex justify-center items-center mx-2"
+              >
+                <FcGoogle className="w-full h-full" />
+              </a>
+              <a
+                href={`http://maps.apple.com/?q=${resultData?.title},${resultData?.address?.city},${resultData?.address?.county},${resultData?.address?.postalCode}`}
+                target="_blank"
+                rel="noreffer"
+                className="w-[25px] h-[25px] flex justify-center items-center mx-2"
+              >
+                <BsApple className="w-[90%] h-[90%]" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div>//Weather</div>
+      </div>
+      {viewMore === true && <div>HALLOO!</div>}
+      <div className="w-[40px] h-[40px] absolute right-[10px] bottom-[10px]">
+        <AiFillDownCircle
+          className={`${
+            viewMore ? 'rotate-180' : 'rotate-0	'
+          } transition-all ease-in-out duration-300 w-full h-full`}
+          onClick={() => {
+            setViewMore((prev) => (!prev ? true : false));
+          }}
+        />
+      </div>
     </section>
   );
 };
--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -13,16 +13,18 @@ const Results = ({ locations }: { locations: any }) => {
     );
   }
   return (
-    <div className="max-w-5xl h-auto mx-auto mt-6 flex flex-col justify-center items-center">
+    <div className="max-w-5xl h-auto min-h-[300px] mx-auto mt-6 flex flex-col justify-center items-center">
       <h1 className="text-2xl font-bold">Results</h1>
       <hr className="border-b border-[1px] border-gray-400 w-full my-4" />
-      {locations.items.map((location: any) => {
-        return (
-          <div key={location.id} className="w-full h-auto my-5">
-            <Result resultData={location} />
-          </div>
-        );
-      })}
+      <div className="grid md:grid-cols-1 lg:grid-cols-2 grid-flow-rows gap-4">
+        {locations.items.map((location: any) => {
+          return (
+            <div key={location.id} className="h-auto min-h-[300px] my-5 mx-5">
+              <Result resultData={location} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
