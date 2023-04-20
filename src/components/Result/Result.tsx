4885ae945a963229474bb753a58116d4329e57fb
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-04-20 01:03:04 +0100

    started adding contact data to result

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,8 +1,61 @@
-import React from 'react';
+import { getMilesFromMeters } from '../../utils/useGetMiles';
 
-const Result = (resultData: any) => {
+const Result = ({ resultData }: { resultData: any }) => {
   console.log(resultData);
-  return <div>Result</div>;
+  return (
+    <section className="w-full min-h-[300px] h-auto flex flex-row justify-start items-center rounded-lg bg-gray-200 p-5">
+      <div>
+        <h2>{resultData.title}</h2>
+        <p>
+          Distance: <span>{getMilesFromMeters(resultData.distance)}</span> Miles
+        </p>
+
+        {resultData?.contacts
+          ? resultData?.contacts.map((contact: any) =>
+              contact?.phone
+                ? contact?.phone.map((phone: any) => {
+                    return (
+                      <p>
+                        Phone:{' '}
+                        <a
+                          className="hover:text-[#4ab836]"
+                          href={`tel:+${phone.value}`}
+                        >
+                          {phone.value}
+                        </a>
+                      </p>
+                    );
+                  })
+                : null
+            )
+          : null}
+
+        {resultData?.contacts
+          ? resultData?.contacts.map((contact: any) =>
+              contact?.fax
+                ? contact?.fax.map((fax: any) => {
+                    return <p>Fax: {fax.value}</p>;
+                  })
+                : null
+            )
+          : null}
+        {resultData?.contacts?.[0]?.www ? (
+          <p>
+            Website:{' '}
+            <a
+              className="hover:text-[#4ab836]"
+              href={resultData?.contacts?.[0]?.www?.[0]?.value}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {resultData?.contacts?.[0]?.www?.[0]?.value}
+            </a>
+          </p>
+        ) : null}
+      </div>
+      <div></div>
+    </section>
+  );
 };
 
 export default Result;
--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -16,9 +16,9 @@ const Results = ({ locations }: { locations: any }) => {
     <div className="max-w-5xl h-auto mx-auto mt-6 flex flex-col justify-center items-center">
       <h1 className="text-2xl font-bold">Results</h1>
       <hr className="border-b border-[1px] border-gray-400 w-full my-4" />
-      {locations.items.map((location: object) => {
+      {locations.items.map((location: any) => {
         return (
-          <div className="max-w-5xl h-auto my-5">
+          <div key={location.id} className="w-full h-auto my-5">
             <Result resultData={location} />
           </div>
         );
