ca74eef2ed6ebd1f0426cd0c3cb8d5d341fdc720
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-04-22 14:34:23 +0100

    added custom map marker icon

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -4,8 +4,7 @@ import L from 'leaflet';
 
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-import { FcGoogle } from 'react-icons/fc';
-import { BsApple } from 'react-icons/bs';
+import MapMarker from '../MapMarker/MapMarker';
 
 const Map = ({
   coords,
@@ -50,64 +49,7 @@ const Map = ({
           <Popup>You are here!</Popup>
         </Marker>
         {searchResults.items.map((result: any) => {
-          console.log(result);
-          return (
-            <Marker
-              key={result.id}
-              icon={DefaultIcon}
-              position={[result.position.lat, result.position.lng]}
-            >
-              <Popup>
-                <div>
-                  <p>{result.title}</p>
-
-                  <a
-                    className="hover:text-[#4ab836] ease-in-out transition-all duration-300 block"
-                    href={`tel:+${result?.contacts?.[0]?.phone?.[0]?.value}`}
-                  >
-                    {result?.contacts?.[0]?.phone?.[0]?.value}
-                  </a>
-
-                  <a
-                    className="hover:text-[#4ab836] ease-in-out transition-all duration-300 block"
-                    href={result?.contacts?.[0]?.email?.[0]?.value}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {result?.contacts?.[0]?.email?.[0]?.value}
-                  </a>
-
-                  <a
-                    className="hover:text-[#4ab836] ease-in-out transition-all duration-300 block"
-                    href={result?.contacts?.[0]?.[0]?.www?.[0]?.value}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {result?.contacts?.[0]?.www?.[0]?.value.slice(11, 50)}
-                  </a>
-                  <div className="flex flex-row jusitfy-space items-center mt-2">
-                    <h5>Navigate:</h5>
-                    <a
-                      href={`https://maps.google.com/?q=${result?.title}?.address?.city},${result?.address?.county},${result?.address?.postalCode}`}
-                      target="_blank"
-                      rel="noreffer"
-                      className="w-[20px] h-[20px] flex justify-center items-center mx-2"
-                    >
-                      <FcGoogle className="w-full h-full" />
-                    </a>
-                    <a
-                      href={`http://maps.apple.com/?q=${result?.title},${result?.address?.city},${result?.address?.county},${result?.address?.postalCode}`}
-                      target="_blank"
-                      rel="noreffer"
-                      className="w-[20px] h-[20px] flex justify-center items-center mx-2"
-                    >
-                      <BsApple className="w-[90%] h-[90%]" />
-                    </a>
-                  </div>
-                </div>
-              </Popup>
-            </Marker>
-          );
+          return <MapMarker result={result} key={result.id} />;
         })}
         <MapView />
       </MapContainer>
