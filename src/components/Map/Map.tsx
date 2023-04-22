9ec5baa6036846bc199dcb36a93e9596dadeea01
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-04-22 15:16:11 +0100

    fixed omport

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -4,7 +4,7 @@ import L from 'leaflet';
 
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-import MapMarker from '../MapMarker/MapMarker';
+import { MapMarker } from '../index';
 
 const Map = ({
   coords,
