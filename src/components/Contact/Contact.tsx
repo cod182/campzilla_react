b0fa3a0b72b4362dd6f250818ea68402eba3d199
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-04-26 13:11:46 +0100

    added reset and sending to contact message

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -19,12 +19,23 @@ const Contact = ({ handleContact }: { handleContact: any }) => {
       )
       .then(
         (result: any) => {
-          console.log(result.text);
-          setMessage(result.text);
+          console.log(result);
+          if (result.status === 200) {
+            setMessage('Message Sent!');
+            form.current[0].value = '';
+            form.current[1].value = '';
+            form.current[2].value = '';
+            setTimeout(function () {
+              setMessage(null);
+            }, 3000);
+          }
         },
         (error: any) => {
           console.log(error.text);
-          setMessage(error.text);
+          setMessage('Failed to send message. Please try again.');
+          setTimeout(function () {
+            setMessage(null);
+          }, 3000);
         }
       );
   };
@@ -32,7 +43,7 @@ const Contact = ({ handleContact }: { handleContact: any }) => {
   return (
     <div className=" w-full h-full sm:w-[50vw] sm:h-[50vh] bg-white rounded-xl relative opacity-[0.9] p-5 flex flex-col justify-center items-center">
       <AiFillCloseCircle
-        className="right-[10px] top-[10px] text-2xl absolute"
+        className="right-[10px] top-[10px] text-2xl absolute hover:text-white hover:bg-blue-500 rounded-full"
         onClick={() => handleContact()}
       />
       <form
@@ -43,6 +54,7 @@ const Contact = ({ handleContact }: { handleContact: any }) => {
         <div className="my-2 flex flex-row justify-around items-center w-full">
           <label className="font-semibold">Name:</label>
           <input
+            required
             className="mx-2 border-black border-[2px] rounded-lg w-full px-2 py-1"
             type="text"
             name="user_name"
@@ -51,6 +63,7 @@ const Contact = ({ handleContact }: { handleContact: any }) => {
         <div className="my-2 flex flex-row justify-around items-center w-full">
           <label className="font-semibold">Email:</label>
           <input
+            required
             className="mx-2 border-black border-[2px] rounded-lg w-full px-2 py-1"
             type="email"
             name="user_email"
@@ -60,7 +73,8 @@ const Contact = ({ handleContact }: { handleContact: any }) => {
         <div className="my-2 flex flex-row justify-around items-center w-full">
           <label className="font-semibold">Message:</label>
           <textarea
-            className="mx-2 border-black border-[2px] rounded-lg w-full px-2 py-1"
+            required
+            className="mx-2 border-black border-[2px] rounded-lg w-full px-2 py-1 min-h-[80px]"
             name="message"
           />
         </div>
